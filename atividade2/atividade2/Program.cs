b3e9c1ac068518ab4a027bce033a158f4156b78e
JhonataVini <35267394+JhonataVini@users.noreply.github.com>
2019-07-21 21:12:59 -0300

    Revert "Initial commit"
    This reverts commit 9513a4d2d7b44a9ffd1fb78e3d88858fdf012195.

---
+++ atividade2/atividade2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace atividade2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // Um funcionário de uma empresa recebe aumento salarial anualmente.Sabe - se que:
+            //Esse funcionário foi contratado em 2005, com salário 
+            //inicial de R$ 1.000,00.Em 2006, ele recebeu aumento de 1,5 % sobre seu salário inicial.
+            // A partir de 2007(inclusive), os aumentos salariais
+            //sempre corresponderam ao dobro do percentual do ano anterior. Faça um programa que
+            //determine o salário atual desse funcionário.
+
+
+           float salario_ini = 1000,
+                salario_total,
+                percentual;
+
+            float funcionario;
+
+            Console.WriteLine(" #################################### ");
+            Console.WriteLine(" Funcionario recebe = " + salario_ini+ " Em 2005");
+            percentual = ((float)15/100)*1000;
+
+            Console.WriteLine(" #################################### ");
+            salario_total = salario_ini + percentual;
+            Console.WriteLine(" Funcionario recebe = " + salario_total + " Em 2006");
+
+            Console.WriteLine(" #################################### ");
+            salario_total = salario_ini + (percentual * 2);
+            Console.WriteLine(" Funcionario recebe = " + salario_total + " Em 2007");
+
+            Console.WriteLine(" #################################### ");
+            Console.WriteLine(" Percentual de reajuste é = "+percentual);
+
+            Console.WriteLine(" #################################### ");
+            Console.ReadKey();
+
+        }
+    }
+}
